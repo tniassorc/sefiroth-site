0fe9ade2d38ba5a5fa87ba6fb84fca57590154c7
tniassorc <jacopoasam@zoho.com>
2025-07-29 01:12:28 +0200

    fixed postlink js

--- static/js/postlinks.js
+++ static/js/postlinks.js
@@ -1,5 +1,8 @@
-var postLinks = document.querySelectorAll('.post a');
-for (var i = 0; i < postLinks.length; i++) {
-  postLinks[i].target = '_blank';
-  postLinks[i].rel = 'noopener noreferrer';
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.post a');
+
+  links.forEach(function (link) {
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+  });
+});
