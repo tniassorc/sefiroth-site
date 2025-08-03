4c821ea88fee2f73728ecb2b946f2a2bf48109d3
tniassorc <jacopoasam@zoho.com>
2025-08-03 23:09:33 +0200

    changed mehr behaviour

--- static/js/collapsible.js
+++ static/js/collapsible.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-     var coll = document.getElementsByClassName("collapsible");
-     var i;
+  var coll = document.getElementsByClassName("collapsible");
+  var sefiinfo = document.querySelector(".sefiinfo"); // select the single element
 
-     for (i = 0; i < coll.length; i++) {
-       coll[i].addEventListener("click", function() {
-         this.classList.toggle("active");
-         var content = this.nextElementSibling;
-         if (content.style.maxHeight){
-           content.style.maxHeight = null;
-         } else {
-           content.style.maxHeight = content.scrollHeight + "px";
-         } 
-       });
-     }
-   });
+  for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+
+      if (sefiinfo) {
+        sefiinfo.classList.toggle("active");
+      }
+
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+});
